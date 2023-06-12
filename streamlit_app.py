00aa4b4f84b3b0f6a9e215620a4d6eaad23dad6b
kaushal <kaushalc64@gmail.com>
2023-06-12 12:08:53 +0530

    query_demo_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,29 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+from langchain.llms import OpenAI
 
-st.write('Hello world!')
+from langchain import PromptTemplate
+
+st.set_page_config(page_title="Query Demo App")
+st.title('Query Demo App')
+openai_api_key = st.sidebar.text_input('OpenAI API key', type='password')
+
+def generate_response(topic):
+	llm = OpenAI(model_name='text-davinci-003', openai_api_key=openai_api_key)
+	template = 'As an experienced hiring manager, tell me the key qualifications required for {job} role.'
+	prompt = PromptTemplate(input_variables=['job'], template=template)
+	prompt_query = prompt.format(topic=topic)
+
+	#run the llm model and return or print out the response
+
+	response = llm(prompt_query)
+	return st.info(response)
+
+with st.form('theform'):
+	topic_text = st.text_input('Enter the keyword:','')
+	submitted = st.form_submit_button('Submit')
+	if not openai_api_key.startswith('sk-'):
+		st.warning('Please enter your OpenAI api key!', icon='⚠')
+	if submitted and openai_api_key.startswith('sk-'):
+		generate_response(topic_text)
+		
