ab3cb9375816954215bc5bab553717e3ec0de6d2
kaushal <kaushalc64@gmail.com>
2023-06-26 18:44:47 +0530

    new updatE

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,14 @@ from langchain.llms import OpenAI
 
 from langchain import PromptTemplate
 
-st.set_page_config(page_title="Query Demo App")
-st.title('Query Demo App')
+st.set_page_config(page_title="Personal AI Career Guide ")
+st.title('Personal Career Guide')
+st.subheader("This is a Personal Career Guiding bot, which helps you in taking neccessary steps to grow, excel or enter into a certain field, just enter your Field of interest and it would help you in properly managing all the stpes to excel in that.")_
 openai_api_key = st.sidebar.text_input('OpenAI API key', type='password')
 
 def generate_response(topic):
 	llm = OpenAI(model_name='text-davinci-003', openai_api_key=openai_api_key)
-	template = 'As an experienced hiring manager, tell me the key qualifications required for {topic} role.'
+	template = 'You are a experienced career guide, hiring manager and role suggestion AI, who helps people on queries related to career. YOur job is to suggest best steps to excel or enter into the {topic_text}, with proper context and source.'
 	prompt = PromptTemplate(input_variables=['topic'], template=template)
 	prompt_query = prompt.format(topic=topic)
 
